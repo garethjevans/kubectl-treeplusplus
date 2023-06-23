bc7144d5bb000cc31dca769786b0d39f3cdddb92
Gareth Evans <gareth@bryncynfelin.co.uk>
2023-06-23 12:13:57 +0100

    don't fail on permission issues

--- cmd/kubectl-tree/rootcmd.go
+++ cmd/kubectl-tree/rootcmd.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -135,7 +135,7 @@ func run(command *cobra.Command, args []string) error {
 	klog.V(2).Infof("querying all api objects")
 	apiObjects, err := getAllResources(dyn, apis.resources(), allNs)
 	if err != nil {
-		return fmt.Errorf("error while querying api objects: %w", err)
+		klog.V(2).Infof("error while querying api objects: %w", err)
 	}
 	klog.V(2).Infof("found total %d api objects", len(apiObjects))
 
